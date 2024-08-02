500c7113c1d10c67470a33a4f12387cf8b9ae426
malikadil <muhammad.adil@consultancy.co.uk>
2024-08-02 16:13:42 +0500

    Commiting error

--- learning-bg/apps/user-account/src/app/controllers/user.controller.ts
+++ learning-bg/apps/user-account/src/app/controllers/user.controller.ts
@@ -4,8 +4,14 @@ import { Controller,  } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RMQ_MESSAGES } from '@shared/constants';
 import {CreateUserDto} from '@shared/dto'
+import { UserService } from '../services/user.service';
+
 @Controller()
 export class UserController {
+
+  constructor(
+    private readonly userService: UserService
+  ) {}
   
   @MessagePattern(RMQ_MESSAGES.HEALTH.HEALTH_CHECK)
   async healthCheck() {
@@ -17,9 +23,15 @@ export class UserController {
 
   @MessagePattern(RMQ_MESSAGES.USER.CREATE)
   async createUser(@Payload() payload: CreateUserDto) {
-    return {
-      message: 'User created successfully',
-    };
+   
+    const { name, email  } = payload
+    const cognitUser = await this.userService.createAdminAccount({
+      name,
+      email
+    })  
+
+    console.log('======= cognitUser',cognitUser);
+    return cognitUser
   }
 
   
